383d5b7ea655698b6ee4e1397f339005d2f99864
meguncasey <meganleo@live.co.uk>
2024-03-25 17:54:37 +0000

    Add files via upload

---
+++ stuff/main.cpp
@@ -0,0 +1,88 @@
+#include <windows.h>
+#define FILE_MENU_NEW 1
+#define FILE_MENU_OPEN 2
+#define FILE_MENU_EXIT 3
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+void AddMenus(HWND);
+
+HMENU hMenu;
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdsshow)
+{
+    WNDCLASSW wc = {0};
+
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if(!RegisterClassW(&wc))
+        return -1;
+
+    CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE,100,100,500,500,
+                  NULL,NULL,NULL,NULL);
+    MSG msg = {0};
+
+    while( GetMessage(&msg, NULL, NULL, NULL) )
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+ {
+     switch ( msg)
+     {
+     case WM_COMMAND:
+
+         switch(wp)
+         {
+         case FILE_MENU_EXIT:
+            DestroyWindow(hWnd);
+            break;
+         case FILE_MENU_NEW:
+             MessageBeep(MB_ICONINFORMATION);
+            break;
+
+         }
+
+        break;
+     case WM_CREATE:
+        AddMenus(hWnd);
+        break;
+     case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+     default:
+      return DefWindowProcW(hWnd,msg,wp,lp);
+
+
+     }
+ }
+void AddMenus(HWND hWnd)
+{
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();
+    HMENU hSubMenu = CreateMenu();
+
+    AppendMenu(hSubMenu,MF_STRING,NULL,"Username Program");
+
+    AppendMenu(hFileMenu,MF_STRING,FILE_MENU_NEW, "new");
+    AppendMenu(hFileMenu,MF_POPUP,(UINT_PTR)hSubMenu, "username");
+    AppendMenu(hFileMenu,MF_SEPARATOR,NULL,NULL);
+    AppendMenu(hFileMenu,MF_STRING, FILE_MENU_OPEN, "EXIT");
+
+    AppendMenu(hMenu,MF_POPUP,(UINT_PTR)hFileMenu,"File");
+    AppendMenu(hMenu,MF_STRING,NULL, "Password");
+
+    SetMenu(hWnd,hMenu);
+
+}
+
+
